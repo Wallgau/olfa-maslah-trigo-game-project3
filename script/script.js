236126712a138da09d45d8b7eeb4763296014c2c
Olfa <maslah.olfa@gmail.com>
2019-02-13 22:53:35 -0500

    positionning radian value, calculate width of each line and organize my js to make it cleanner, js: myApp.init(), event on click make work the first part

--- script/script.js
+++ script/script.js
@@ -1,28 +1,49 @@
-$(document).ready(function (){
-        //when document is ready, on click of the button with a class='fa-play-circle' prevent the default event
-        $('.fa-play-circle').on('click', function (event) {
-                event.preventDefault();
-                console.log('play start!');
-                //target all line and make them desappear slowly with fadeOut('slow');
-                $('.line-red').fadeIn(5000,function(){
-                        $('.line-red').fadeOut(10000);
-                        $('.line-blue').fadeIn(10000, function(){
-                                $('.line-blue').fadeOut(5000);
-                                $('.line-green').fadeIn(10000,function(){
-                                        $('line-green').fadeOut(5000);
-                                        $('.trait').fadeIn(5000);
-                                });
-                        });
-                        
-                }); 
-                /* $('.line-blue').fadeIn(5000, function () {
-                        $('.line-blue').fadeOut(5000);
-                }); */     
-               
+
+    //when document is ready, on click of the button with a class='fa-play-circle' prevent the default event
+const myApp = {};
+//on créer une fonction sur myApp pour organiser correctement le code, ici myApp contient ce qui arrive dans myApp.setup
+myApp.init = function() {
+    console.log('initialize');
+    myApp.setup();
+};
+//myApp est utilisé pour prévenir l'evenement par defaut
+myApp.setup = function() {
+    $('.fa-play-circle').on('click', function (event) {
+        event.preventDefault();
+        console.log('play start!');
+        //ici on appelle les fonctions qui se produisent au click du button start
+        myApp.drawShapes();
+        myApp.startTimer();
+    });
+}
+
+myApp.drawShapes = function() {
+    //faire apparaitre et disparaitre les line par class au fur et à mesure
+    $('.line-red').fadeIn(1000, function () {
+        $('.line-red').fadeOut(10000);
+        $('.line-blue').fadeIn(10000, function () {
+            $('.line-blue').fadeOut(10000);
+            $('.line-green').fadeIn(10000, function () {
+                $('line-green').fadeOut(10000);
+                $('.trait').fadeIn(10000);
+            });
         });
+    });
+}
+
+myApp.startTimer = function() {
+    console.log('start counter');
+}
+
+$(document).ready(function () {
+    myApp.init();
+})
+
+        
+        
 
                 
-});
-             
+
+
 
         
