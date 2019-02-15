2628a902df8d10119cc6dc71e05b5081229a33c4
Olfa <maslah.olfa@gmail.com>
2019-02-15 15:44:54 -0500

    enlever le commentaire et maintenant ne fonctionne plus

--- script/script.js
+++ script/script.js
@@ -13,12 +13,13 @@ myApp.setup = function () {
     /* myApp.drawShapes(); */
     myApp.startTimer();
     myApp.randomTrait();
+    myApp.drawShapes();
 
 };
 
 
 
-/* myApp.drawShapes = function () {
+myApp.drawShapes = function () {
     //faire apparaitre et disparaitre les line par class au fur et à mesure
     $('.line-red').fadeIn(1000, function () {
         $('.line-red').fadeOut(10000);
@@ -32,43 +33,43 @@ myApp.setup = function () {
             });
         });
     });
-}; */
-
+};
 
 
 
 
 myApp.randomTrait = function () {
 
-    $('button .fa-play-circle').on('click', function (event) {
+    $('button.play').on('click', function (event) {
         event.preventDefault();
         //je cherche à obtenir un nombre en demandant à js combien d'élément avec la class .trait sil y a (length), ce chiffre me permet ensuite d'utiliser Math.floor(Math.random()) pour avoir un chiffre entier aléatoirement
+        $('.active').removeClass('active');
         const sizeTrait = $('.trait').length;
-        const randNumb = Math.floor(Math.random() * sizeTrait);
+        const randNumb = Math.floor(Math.random() * sizeTrait) + 1;
         const randTest = $(`.trait:nth-child(${randNumb})`);
-        randTest.addClass('active').css('display', 'block');//Even if I put display:block on my css, I need to add it there because it doesn't listen, comme j'ai déjà un display:none sur cette div avec une autre class.
+        randTest.addClass('active');//Even if I put display:block on my css, I need to add it there because it doesn't listen, comme j'ai déjà un display:none sur cette div avec une autre class.
         myApp.randomValue();
-        myApp.randCos();
+        /*  myApp.randCos(); */
     })
 
 
 };
 //make one Pi value become white, randmoly 
 myApp.randomValue = function () {
-
+    $('.active-question').removeClass('active-question');
     const sizeAngleValue = $('.active .pi').length;
-    const randNumber = Math.floor(Math.random() * sizeAngleValue);
+    const randNumber = Math.floor(Math.random() * sizeAngleValue) + 1;
     const randPi = $(`.active .pi:nth-of-type(${randNumber})`);
-    randPi.css({ 'color': 'white', 'border': '1px solid black' });
+    randPi.addClass('active-question');
 
 }
 //make one cos become white randomly
 myApp.randCos = function () {
     const sizeCosValue = $('.active .cos').length;
     const randNum = Math.floor(Math.random() * sizeCosValue) + 1;
-    console.log(randNum);
+
     const randCos = $(`.active .cos:nth-of-type(${randNum})`);
-    console.log(randCos);
+
     randCos.css({ 'color': 'white', 'border': '1px solid black' });
 }
 
