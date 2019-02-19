7a63ea1200912bb823777e048da3e5a934142186
Olfa <maslah.olfa@gmail.com>
2019-02-19 08:26:34 -0500

    remove position absolute footer et translate comments from french to english

--- script/script.js
+++ script/script.js
@@ -2,15 +2,13 @@
 const answersPiList = ["π/6", "π/4", "π/3", "2π/3", "3π/4", "5π/6", "7π/6", "5π/4", "4π/3", "3π/2", "5π/3", "7π/4", "11π/6"];
 //when document is ready, on click of the button with a class='fa-play-circle' prevent the default event
 const myApp = {};
-//on créer une fonction sur myApp pour organiser correctement le code, ici myApp contient ce qui arrive dans myApp.setup
+//myApp fonction is created to organise the document. init will contain the setup function
 myApp.init = function () {
 
     myApp.setup();
 };
-//myApp est utilisé pour prévenir l'evenement par defaut
-myApp.setup = function () {
 
-    //ici on appelle les fonctions qui se produisent au click du button start
+myApp.setup = function () {
 
     myApp.randomTrait();
     myApp.drawShapes();
@@ -21,7 +19,7 @@ myApp.setup = function () {
 myApp.drawShapes = function () {
     $('#start').css('display', 'none');
     $('.questions').css('display', 'none');
-    //faire apparaitre et disparaitre les line par class au fur et à mesure
+    //make the lines fadeIn/fadeOut in sequence
     $('.line-red').fadeIn(5000, function () {
         $('.line-red').fadeOut(1000);
         $('.line-blue').fadeIn(5000, function () {
@@ -45,7 +43,8 @@ myApp.randomTrait = function () {
 
     $('button.play').on('click', function (event) {
         event.preventDefault();
-        //je cherche à obtenir un nombre en demandant à js combien d'élément avec la class .trait sil y a (length), ce chiffre me permet ensuite d'utiliser Math.floor(Math.random()) pour avoir un chiffre entier aléatoirement
+
+        //Get the length of the list of all the element with the class trait to get an random number between 0 and that value
 
         $('.memorise-test').css('display', 'none');
         $('.trait').css('display', 'none');
@@ -55,7 +54,7 @@ myApp.randomTrait = function () {
         const randTest = $(`.trait:nth-child(${randNumb})`);
         //ajoute attr car jQuery manipule le DOM, our contourner le inline css
         randTest.addClass('active').attr('style', '');
-        //Even if I put display:block on my css, I need to add it there because it doesn't listen, comme j'ai déjà un display:none sur cette div avec une autre class.
+        //JQuery fadeOut add a style attr (DOM manipulation). Need to empty the style attribute for the css display property to work
         myApp.randomValue();
         /*  myApp.randCos(); */
     })
@@ -118,7 +117,7 @@ myApp.feedBack = function () {
         e.preventDefault();
 
         const isAnswered = $('.feedback-user').hasClass('submitted');
-        //to handle error, if the user has already made a choice and try to click on other answer
+        //to prevent unexpected behavior, if the user has already made a choice and try to click on other answer, it is blocking it
         if (isAnswered === true) {
             alert('You have already made a choice, please press play to continue')
         }
